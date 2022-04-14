c1b50732f211ab4a21bec981dcc335718ff19c21
Andre Parada <andreparada98@gmail.com>
2022-04-13 23:57:20 -0400

    Add frist configurations of  CRUD of Schedule

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const port = process.env.port || 3000                   //Define a porta
 
 const routes = {                        //Configurando as rotas da API
     '/': 'Pagina Inicial',
-    '/create': 'Cadastro de usuario',
+    '/schedule': 'Cadastro de agendamento',
     '/list' : 'Listagem de agendamentos'
 }
 
 app.listen(port, () => {                                          // Chama o arquivo App.JS e escuta ele.
     console.log(`Servidor escutando em http://localhost:${port}`)   
-})
+});
